c8b4598479183c8ee8938512c700b23daad53f22
Ankita-Thakur20 <ankitathakur036238@gmail.com>
2024-10-06 10:38:19 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,131 @@
+
+// var a = 5;
+// if(a % 2===0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+// a=254;
+// b=254;
+// if(a>b){
+//     console.log("a is greater");
+// }
+// else if(a===b){
+//     console.log("equal");
+// }
+// else{
+//     console.log("b is greater");
+// }
+
+
+// const year="1999";
+// if(year %4===0){
+   
+
+//   if(year %100===0){
+   
+    
+//     if(year %400===0){
+
+//         console.log("yes its a leap year");
+
+//     }
+//     else{
+//         console.log("Its not a leap year");
+//     }
+// } else{
+//     console.log("Its a leap year");
+// }
+
+// } else{
+//     console.log("Its not a leap year");
+// }
+
+// if else
+
+
+
+
+// switch case
+
+// var day="hg";
+// switch(day){
+// case "monday":
+//     console.log("its monday");
+//     break;
+
+// case "tuesday":
+//     console.log("its tuesday");
+//     break;
+
+// case "wednesday":
+//     console.log("its wednesday");
+//     break;   
+    
+// default:
+//     console.log("invalid");  
+
+// }
+ 
+var alphabet="u";
+switch(alphabet){
+
+case "a":
+    console.log("vowel");
+    break;
+
+case "e":
+    console.log("vowel");
+    break;   
+
+case "i":
+    console.log("vowel");
+    break;  
+    
+case "o":
+    console.log("vowel");
+    break; 
+
+case "u":
+    console.log("vowel");
+    break;
+
+default:
+    console.log("consonent") ;                 
+}
+
+// console.log(x);
+
+// non premitive datatypes
+
+// let b=[1,2,3];
+// console.log(b[2]);
+
+// let c=[1,2,4,"hi","world",["w","r","t"]];
+// console.log(c[5][2]);
+// console.log(c[3]);
+// c.push(200);
+// console.log(c);  
+// add to end
+
+// c.pop();
+// console.log(c);
+// del from end
+
+// c.shift();
+// console.log(c);
+// del from start
+
+// c.unshift(0);
+// console.log(c);
+// add in start
+
+// console.log(c.length);
+// length of array
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [1, 2, 3];
+
+// console.log(arr1 === arr2);
